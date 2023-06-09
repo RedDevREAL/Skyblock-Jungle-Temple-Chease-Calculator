3b831f2dce6c330041c5cc2509bca52a69de5ae6
RedDevREAL <122028605+RedDevREAL@users.noreply.github.com>
2023-06-09 12:27:02 +0200

    Update main.py
    Added Try Except statement for ValueError if coords are null or String

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 
-x = int(input("What is the x coordinate of the  left jungle temple guard? "))
-y = int(input("What is the y coordinate of the  left jungle temple guard? "))
-z = int(input("What is the z coordinate of the  left jungle temple guard? "))
+try:
+    x = int(input("\033[92mWhat is the x coordinate of the  left jungle temple guard? "))
+    y = int(input("\033[92mWhat is the y coordinate of the  left jungle temple guard? "))
+    z = int(input("\033[92mWhat is the z coordinate of the  left jungle temple guard? "))
+except ValueError:
+    print("\n\033[91mCoordinate must be Number!")
+    input("Press Enter to close this window...")
+    exit()
 
 #Debug
-after = False
+done = False
 
-x = x + 61
-y = y - 44
-z = z + 18
+x += 61
+y -= 44
+z += 18
 
 #Debug
-after = True
+done = True
 
-coords_after: str = str(x) + " " + str(y) + " " + str(z)
+coords_after = str(x) + " " + str(y) + " " + str(z)
 
 print()
-print("If you have the mod skytils, then use the following command to set the waypoint:")
+print('If you have the mod "Skytils", then use the following command to set the waypoint:')
 print("/sthw set " + coords_after + " NAME_HERE")
 print()
 print("If you dont have the mod, here are the coordinates:")
